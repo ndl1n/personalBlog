e2532645fc95c214c3e1bddace0a0af62581ec10
ndl1n <a0938391667@gmail.com>
2024-07-18 10:57:25 +0800

    update

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,15 +1,42 @@
-// import { defineStore } from 'pinia'
+import { defineStore } from 'pinia'
 
-// export const useUserStore = defineStore('user', {
-//   state: () => ({
-//     currentUser: null // 儲存當前登入使用者的資料
-//   }),
-//   actions: {
-//     setCurrentUser(user) {
-//       this.currentUser = user
-//     },
-//     clearCurrentUser() {
-//       this.currentUser = null // 登出時清除使用者資料
-//     }
-//   }
-// })
+interface User {
+  account: string
+  address: string
+  email: string
+  id: number
+  name: string
+  password: string
+  phonenum: string
+}
+
+interface UserInfo {
+  name: string
+  account: string
+  email: string
+  phonenum: string
+  address: string
+}
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: {} as User | null // 儲存當前登入使用者的資料
+  }),
+  getters: {
+    userInfo: (state): UserInfo | null => {
+      if (state.user) {
+        const { name, account, email, phonenum, address } = state.user
+        return { name, account, email, phonenum, address }
+      }
+      return null
+    }
+  },
+  actions: {
+    setUser(user: User) {
+      this.user = user
+    },
+    clearUser() {
+      this.user = null // 登出時清除使用者資料
+    }
+  }
+})
